8f842f38ce21cc3802e4ea3e33f24a0c55a8f1c0
mwingerden <matthew.wingerden@gmail.com>
2024-05-29 12:31:38 -0700

    Added Hit Detection and reaction to enemy.

--- Scripts/player_single_bullet.gd
+++ Scripts/player_single_bullet.gd
@@ -4,9 +4,17 @@ var speed = 700
 var direction: Vector2
 @onready var animation = $AnimationPlayer
 
+func _ready():
+	animation.play("fire")
+
 func _process(delta):
 	position += speed * direction * delta
-	animation.play("fire")
 
 func _on_timer_timeout():
 	queue_free()
+
+
+func _on_area_entered(area):
+	if area.name == "EnemyHurtBox":
+		#print("player hit")
+		queue_free()
